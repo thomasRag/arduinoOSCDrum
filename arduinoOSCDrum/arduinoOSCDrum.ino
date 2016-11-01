c0e39dc7a8c14ae6f4490550bb423eab87ed6ec2
thomasRag <tomrag@mail.com>
2016-11-01 09:45:17 -0400

    wip

--- arduinoOSCDrum/arduinoOSCDrum.ino
+++ arduinoOSCDrum/arduinoOSCDrum.ino
@@ -1,8 +1,3 @@
-/*
-   Make an OSC message and send it over UDP
-
-   Adrian Freed
-*/
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include <SPI.h>
@@ -13,22 +8,32 @@ EthernetUDP Udp;
 // define what will be the communication mode
 const int isOSCmode = 0; // mode 0: serial, mode 1: osc
 
-//the Arduino's IP
+/*
+  CONFIGURATION ARDUINO
+*/
+// Arduino IP et mac adress
 IPAddress ip(192, 168, 0, 7);
-// isadora IP. todo: receive it by udp ?
+byte mac[] = { 0xDE, 0xAD, 0xAE, 0xEF, 0xFE, 0xED };
+
+// isadora IP et port
 IPAddress outIp(10, 0, 1, 13);
 const unsigned int outPort = 1234;
-byte mac[] = { 0xDE, 0xAD, 0xAE, 0xEF, 0xFE, 0xED }; // you can find this written on the board of some Arduino Ethernets or shields
 
-//const int knockSensors[] = {0,1};
-const int threshold[] = {300,500};  // threshold value to decide when the detected sound is a knock or not
-
+/*
+  CONFIGURATION CAPTEUR
+*/
+// nombre de piezo devant être lu
 int nbSensors = 2;
+
+// threshold : valeurs seuils à laquel les capteurs doivent envoyer une valeur.
+const int threshold[] = {300,500};
+
+
+int triggerable[];
 
 void  initializeOSC(){
   Serial.println("OSC test");
-  // Ethernet.begin(mac, ip);
-  if (Ethernet.begin(mac) == 0) {
+  if (Ethernet.begin(mac, ip) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
     while(true);
@@ -38,9 +43,8 @@ void  initializeOSC(){
   for (byte thisByte = 0; thisByte < 4; thisByte++) {
     // print the value of each byte of the IP address:
     Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print("."); 
+    Serial.print(".");
   }
-  //this line needed 
   Udp.begin(8888);
 }
 
@@ -56,6 +60,7 @@ void sendOSC(int sensorReading, int pin){
 void sendAnalog(int pin){
   int sensorReading = analogRead(pin);
   if ( sensorReading > threshold[pin]){
+    triggerable[i] = 1;
     Serial.print( "switch");
     Serial.print(pin);
     Serial.print( ":");
@@ -67,14 +72,15 @@ void sendAnalog(int pin){
 }
 
 void setup() {
- //this setup section of code from http://trippylighting.com/teensy-arduino-ect/touchosc-and-arduino-oscuino/
- //this to begin serial
-  Serial.begin(9600); //9600 for a "normal" Arduino board (Uno for example). 115200 for a Teensy ++2
+  Serial.begin(9600);
   if(isOSCmode){
       initializeOSC();
   }
   Serial.print("nbSensors:");
   Serial.println(nbSensors);
+  for(int i=0; i<nbSensors; i++){
+    triggerable[i]= 0;
+  }
 }
 
 
