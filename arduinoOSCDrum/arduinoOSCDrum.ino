1778eb97021a59412f830ff4a63bb934538531e8
thomasRag <tomrag@mail.com>
2016-11-17 23:46:10 -0500

    audio version reading mp3 files

--- arduinoOSCDrum/arduinoOSCDrum.ino
+++ arduinoOSCDrum/arduinoOSCDrum.ino
@@ -1,26 +1,16 @@
-#include <Ethernet.h>
-#include <EthernetUdp.h>
 #include <SPI.h>
-#include <OSCMessage.h>
 
+//Add the SdFat Libraries
+#include <SdFat.h>
+#include <SdFatUtil.h>
+
+//and the MP3 Shield Library
+#include <SFEMP3Shield.h>
+
+SdFat sd;
+SFEMP3Shield MP3player;
 EthernetUDP Udp;
 
-
-/*
-  CONFIGURATION ARDUINO (à adapter)
-*/
-
-// mode osc ou serial pour les test
-const int isOSCmode = 1; // mode 0: serial, mode 1: osc
-
-// Arduino IP et mac adress
-IPAddress ip(192, 168, 0, 7);
-byte mac[] = { 0xDE, 0xAD, 0xAE, 0xEF, 0xFE, 0xED };
-
-// isadora IP et port
-int isDHCP = 0;
-IPAddress outIp(192, 168, 0, 13);
-const unsigned int outPort = 1234;
 /*
   CONFIGURATION CAPTEUR (à adapter)
 */
@@ -28,7 +18,7 @@ const unsigned int outPort = 1234;
 int nbSensors = 6;
 
 // threshold : valeurs seuils à laquel les capteurs doivent envoyer une valeur. (un par type de capteur)
-const int threshold[] = {200,200, 200, 200,200,200};
+const int threshold[] = {100,100, 200, 200,200,200};
 
 int customDelay = 20;
 
@@ -37,48 +27,8 @@ int customDelay = 20;
 */
 int triggerable[] = {};
 
-void  initializeOSC(){
-  Serial.println("OSC test");
-  if (isDHCP == 1 && Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    while(true);
-  }
-  else {
-    Ethernet.begin(mac, ip);
-  }
-  // print your local IP address:
-  Serial.print("Arduino IP address: ");
-  for (byte thisByte = 0; thisByte < 4; thisByte++) {
-    // print the value of each byte of the IP address:
-    Serial.print(Ethernet.localIP()[thisByte], DEC);
-    Serial.print(".");
-  }
-  Udp.begin(8888);
-}
-
-void sendOSC(int sensorReading, int pin){
-    char address[10];
-    sprintf(address, "/switch/%d", pin);
-    OSCMessage msg(address);
-    msg.add((int32_t)sensorReading);
-    Udp.beginPacket(outIp, outPort);
-    msg.send(Udp); // send the bytes to the SLIP stream
-    Udp.endPacket(); // mark the end of the OSC Packet
-    msg.empty(); // free space occupied by message
-}
-
-void sendAnalog(int pin, int sensorValue){
-    Serial.print( "switch");
-    Serial.print(pin);
-    Serial.print( ":");
-    Serial.println(sensorValue);
-    if (isOSCmode) {
-      sendOSC(sensorValue, pin);
-    }
-}
-
 void setup() {
+
   Serial.begin(9600);
   if(isOSCmode){
       initializeOSC();
@@ -88,17 +38,37 @@ void setup() {
   for(int i=0; i<nbSensors; i++){
     triggerable[i]= 1;
   }
-  Serial.print("threshold2:");
-  Serial.println(threshold[0]);
-}
+  Serial.print("thresholds:");
+  for(int i=0; i<nbSensors; i++){
+   Serial.println(threshold[i]);
+  }
 
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_FULL_SPEED)) sd.initErrorHalt();
+  // depending upon your SdCard environment, SPI_HAVE_SPEED may work better.
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+  
+  //Initialize the MP3 Player Shield
+  uint8_t result = MP3player.begin();
+  //check result, see readme for error codes.
+  if(result != 0) {
+    Serial.print(F("Error code: "));
+    Serial.print(result);
+    Serial.println(F(" when trying to start MP3 player"));
+    if( result == 6 ) {
+      Serial.println(F("Warning: patch file not found, skipping.")); // can be removed for space, if needed.
+      Serial.println(F("Use the \"d\" command to verify SdCard can be read")); // can be removed for space, if needed.
+    }
+  }
+}
 
 void loop(){
   for(int i=0; i<nbSensors; i++){
     int sensorReading = analogRead(i);
     if ( sensorReading > threshold[i]){
       if (triggerable[i] == 1){
-        sendAnalog(i, sensorReading);
+        MP3player.playTrack(i+1);
+        Serial.print("playing");
         triggerable[i] = 0;
       }
     }
